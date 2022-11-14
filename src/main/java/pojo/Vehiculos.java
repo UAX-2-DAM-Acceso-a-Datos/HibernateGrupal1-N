27847ee6bce6d426df8abeaa44d16bafd6ac0784
Gonxos <gonjibra@gmail.com>
2022-11-14 12:56:41 +0100

    no message

--- src/main/java/pojo/Vehiculos.java
+++ src/main/java/pojo/Vehiculos.java
@@ -7,6 +7,22 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-public class Vehiculo {
-
+@Entity
+@Table(name = "Vehiculos")
+public class Vehiculos {
+	
+	@Id
+	@Column(name = "dni")
+	private String dni;
+	
+	@Column(name = "matricula")
+	private String matricula;
+	
+	@Column(name = "marca")
+	private String marca;
+	
+	@Column(name = "modelo")
+	private String modelo;
+	
+	
 }
