a40dcb62681d03d84a2bbf3e9bc42684cf17c67d
Slacllop <https://github.com/Slacllop>
2022-11-15 08:48:21 +0100

    Rellenar PropietarioDAO

--- src/main/java/DAO/PropietarioDAO.java
+++ src/main/java/DAO/PropietarioDAO.java
@@ -1,5 +1,63 @@
 package DAO;
+import javax.persistence.Column;
+	import javax.persistence.Entity;
+	import javax.persistence.Id;
+	import javax.persistence.JoinColumn;
+	import javax.persistence.ManyToOne;
+	import javax.persistence.Table;
 
-public class PropietarioDAO {
+	@Entity
+	@Table(name = "Propietario")
+	public class PropietarioDAO {
 
+		@Id
+		@Column(name = "dni")
+		private String dni;
+
+		@Column(name = "nombre")
+		private String nombre;
+
+		@Column(name = "apellido")
+		private String apellido;
+
+		@ManyToOne
+		@JoinColumn(name = "vehiculo", nullable = false)
+		private VehiculoDAO vehiuclo;
+		
+		public PropietarioDAO() {
+			
+		}
+
+		public String getDni() {
+			return dni;
+		}
+
+		public void setDni(String dni) {
+			this.dni = dni;
+		}
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public String getApellido() {
+			return apellido;
+		}
+
+		public void setApellido(String apellido) {
+			this.apellido = apellido;
+		}
+
+		public VehiculoDAO getVehiuclo() {
+			return vehiuclo;
+		}
+
+		public void setVehiuclo(VehiculoDAO vehiuclo) {
+			this.vehiuclo = vehiuclo;
+		}
+	
 }
