a163af35f5711613356751126a003712da47560a
Gonxos <gonjibra@gmail.com>
2022-11-15 10:54:51 +0100

    Añado Propietario y Vehiculo al paquete Pojo

---
+++ src/main/java/pojo/Propietario.java
@@ -0,0 +1,63 @@
+package pojo;
+import javax.persistence.Column;
+	import javax.persistence.Entity;
+	import javax.persistence.Id;
+	import javax.persistence.JoinColumn;
+	import javax.persistence.ManyToOne;
+	import javax.persistence.Table;
+
+	@Entity
+	@Table(name = "Propietario")
+	public class Propietario {
+
+		@Id
+		@Column(name = "dni")
+		private String dni;
+
+		@Column(name = "nombre")
+		private String nombre;
+
+		@Column(name = "apellido")
+		private String apellido;
+
+		@ManyToOne
+		@JoinColumn(name = "vehiculo", nullable = false)
+		private Vehiculo vehiuclo;
+		
+		public Propietario() {
+			
+		} 
+
+		public String getDni() {
+			return dni;
+		}
+
+		public void setDni(String dni) {
+			this.dni = dni;
+		}
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public String getApellido() {
+			return apellido;
+		}
+
+		public void setApellido(String apellido) {
+			this.apellido = apellido;
+		}
+
+		public Vehiculo getVehiuclo() {
+			return vehiuclo;
+		}
+
+		public void setVehiuclo(Vehiculo vehiuclo) {
+			this.vehiuclo = vehiuclo;
+		}
+	
+}
