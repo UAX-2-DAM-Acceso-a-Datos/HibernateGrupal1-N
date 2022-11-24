7e3b5c30b96c2aaa60ce5a8c998fb9138e4731ad
Gonxos <gonjibra@gmail.com>
2022-11-24 12:04:52 +0100

    PropietarioDAO terminado

--- src/main/java/DAO/PropietarioDAO.java
+++ src/main/java/DAO/PropietarioDAO.java
@@ -6,69 +6,69 @@ import org.hibernate.Session;
 
 import IDAO.IPropietarioDAO;
 import pojo.Propietario;
+import pojo.Vehiculo;
 import utils.HibernateUtils;
 
 public class PropietarioDAO implements IPropietarioDAO {
 
 	// Método para añadir un propietario en la BBDD
 	public boolean addPropietario(Propietario p) {
-		boolean success = false;
-		Session session = HibernateUtils.getSessionFactory().openSession();
-		Propietario existente = getPropietarioByDni(p.getDni());
-		if (existente == null) {
-			session.save(p); // declaramos la query de inserción que mete el objeto que recibe (p)
-			session.beginTransaction().commit(); // se ejecuta la query y se guardan los cambios
-			success = true;
-		}
-		session.close();
-		return success;
-	}
+        boolean resul=false;
+        Session session = HibernateUtils.getSessionFactory().openSession();
+        if (p.getDni() != null) {
+            session.beginTransaction();
+            session.save(p);
+            session.getTransaction().commit();
+            resul=true;
+        }
+        session.close();
+        return resul;
+    }
 
 	// Método para actualizar un propietario en la BBDD
 	public boolean updatePropietario(Propietario p) {
-		boolean success = false;
-		Session session = HibernateUtils.getSessionFactory().openSession(); // abrimos la conexión
-		Propietario updated = getPropietarioByDni(p.getDni()); // creamos un objeto Propietario vacio
-		if(updated != null) {
-			updated.setDni(p.getDni()); // Le metemos los valores del objeto anterior para que se actualice con un nuevo
-			updated.setNombre(p.getNombre());
-			updated.setApellido(p.getApellido());
-			session.update(p); // declaramos la query de actualización que mete el nuevo objeto
+		boolean resul = false;
+		Session session = HibernateUtils.getSessionFactory().openSession();
+		Propietario mod = getPropietarioByDni(p.getDni());
+		if (mod != null) {
+			mod.setNombre(p.getNombre());
+			mod.setApellido(p.getApellido());
+
+			session.update(mod);
 			session.beginTransaction().commit();
-			success = true;
+			resul=true;
 		}
 		session.close();
-		return success;
+		return resul;
 	}
 
 	// Método para eliminar un propietario en la BBDD
 	public boolean deletePropietario(String dni) {
-		boolean success = false;
+		boolean resul=false;
 		Session session = HibernateUtils.getSessionFactory().openSession();
 		Propietario deleted = getPropietarioByDni(dni);
-		
-		if(deleted != null) {
+		if (deleted != null) {
 			session.delete(deleted);
 			session.beginTransaction().commit();
-			success = true;
+			resul=true;
 		}
 		session.close();
-		return success;
+		return resul;
 	}
 
 	// Método para obtener todos los propietarios en la BBDD
 	public List<Propietario> getAllPropietario() {
 		Session session = HibernateUtils.getSessionFactory().openSession();
 		session.beginTransaction();
-		List<Propietario> vs = session.createQuery("From vehiculos").list();
-		for (Propietario v : vs) {
-			System.out.println(v);
+		List<Propietario> vs = session.createQuery("From Propietario").list();
+		for (Propietario p : vs) {
+			System.out.println(p);
 		}
 		session.close();
 		return vs;
 
 	}
-
+	
 	// Método para obetener de la BBDD un propietario con un dni específico
 	private Propietario getPropietarioByDni(String dni) {
 		Propietario p = null; // se crea un objeto propietario null
